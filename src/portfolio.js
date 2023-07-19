d356da5fb183f4d68cf13b45de9bb45a8f2caed2
rafi7391 <rafi37230@gmail.com>
2023-07-19 11:57:10 +0530

    added new files

--- src/portfolio.js
+++ src/portfolio.js
@@ -219,7 +219,7 @@ const workExperiences = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  showGithubProfile: true, // Set true or false to show Contact profile using Github, defaults to true
   display: true // Set false to hide this section, defaults to true
 };
 
